5bbcd1e5482e33f543c5e38420cce68e10fe7f28
bingo <blackbing@gmail.com>
2016-03-29 14:14:37 +0800

    follow lint rackt

--- src/index.js
+++ src/index.js
@@ -1,74 +1,74 @@
 /* eslint func-names: 0*/
 /* eslint no-unused-expressions: 0*/
 
-import scrollTop from 'dom-helpers/query/scrollTop';
-import on from 'dom-helpers/events/on';
-import throttle from 'lodash.throttle';
-import { canUseDOM } from 'exenv';
+import scrollTop from 'dom-helpers/query/scrollTop'
+import on from 'dom-helpers/events/on'
+import throttle from 'lodash.throttle'
+import { canUseDOM } from 'exenv'
 
-const prefix = '@@POS';
-const scrollThreshold = 50;
+const prefix = '@@POS'
+const scrollThreshold = 50
 
 function saveState(key, y) {
-  sessionStorage.setItem(`${prefix}${key}`, `${y}`);
+  sessionStorage.setItem(`${prefix}${key}`, `${y}`)
 }
 function getState(key) {
-  return sessionStorage.getItem(`${prefix}${key}`);
+  return sessionStorage.getItem(`${prefix}${key}`)
 }
 function createKey(loc) {
-  const query = loc.query;
-  const queryString = Object.keys(query).map(k => `${encodeURIComponent(k)}=${encodeURIComponent(query[k])}`).join('&');
-  return `${loc.pathname}?${queryString}`;
+  const query = loc.query
+  const queryString = Object.keys(query).map(k => `${encodeURIComponent(k)}=${encodeURIComponent(query[k])}`).join('&')
+  return `${loc.pathname}?${queryString}`
 }
 
 function scrollToHash(loc) {
   //
   if (!loc.hash) {
-    return false;
+    return false
   }
-  const hash = decodeURIComponent(loc.hash);
-  const hashParts = hash.split('#');
+  const hash = decodeURIComponent(loc.hash)
+  const hashParts = hash.split('#')
 
   if (hashParts.length >= 2) {
-    const _hash = hashParts[1];
-    const element = document.querySelector(`#${_hash}`);
+    const _hash = hashParts[1]
+    const element = document.querySelector(`#${_hash}`)
     if (element) {
-      element.scrollIntoView();
-      return true;
+      element.scrollIntoView()
+      return true
     }
   }
-  return false;
+  return false
 }
 
 function scrollToState(key) {
-  const y = getState(key);
+  const y = getState(key)
   if (!y) {
-    return false;
+    return false
   }
   // setTimeout 0 for avoid page rendering
   setTimeout( () => {
-    window.scrollTo(0, y);
-  }, 0);
-  return true;
+    window.scrollTo(0, y)
+  }, 0)
+  return true
 }
 
-export default function(history) {
+export default function (history) {
   if (!canUseDOM) {
-    return history;
+    return history
   }
-  let currentKey = null;
-  history.listen( function(loc) {
-    currentKey = createKey(loc);
+  let currentKey = null
+  history.listen( function (loc) {
+    currentKey = createKey(loc)
     // first try to check hash
     // second try to scrollTo State
     // finally scrollToTop
-    !scrollToHash(loc) && !scrollToState(currentKey) && window.scrollTo(0, 0);
-  });
+    !scrollToHash(loc) && !scrollToState(currentKey) && window.scrollTo(0, 0)
+  })
   const onScroll = () => {
-    const y = scrollTop(window);
-    saveState(currentKey, y);
-  };
+    const y = scrollTop(window)
+    saveState(currentKey, y)
+  }
 
-  on(window, 'scroll', throttle(onScroll, scrollThreshold));
-  return history;
+  on(window, 'scroll', throttle(onScroll, scrollThreshold))
+  return history
 }
