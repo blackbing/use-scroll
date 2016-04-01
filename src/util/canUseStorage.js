c928d487730c52ae1dcf18d083235258745de8d6
bingo <blackbing@gmail.com>
2016-04-01 19:59:46 +0800

    fix build error and release 0.1.3

--- src/util/canUseStorage.js
+++ src/util/canUseStorage.js
@@ -1,14 +1,13 @@
 
 let canUseStorage = true
-const canUseStorageKey = 'canUseStorageTest'
-(function () {
-  try {
-    sessionStorage.setItem(canUseStorageKey, 'test')
-    sessionStorage.getItem(canUseStorageKey)
-    sessionStorage.removeItem(canUseStorageKey)
-  } catch (e) {
-    canUseStorage = false
-  }
-})()
+const canUseStorageKey = 'test'
+
+try {
+  sessionStorage.setItem(canUseStorageKey, 'test')
+  sessionStorage.getItem(canUseStorageKey)
+  sessionStorage.removeItem(canUseStorageKey)
+} catch (e) {
+  canUseStorage = false
+}
 
 export default canUseStorage
